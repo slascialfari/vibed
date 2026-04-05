a234f9bfc81d3bc4820003cb4408adcac5bb7ad5
Stefano Lascialfari <stefano@users.noreply.github.com>
2026-04-05 18:46:24 +0200

    Initial Commit

---
+++ Vibed/VibedApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibedApp.swift
+//  Vibed
+//
+//  Created by Stefano Lascialfari on 05/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct VibedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
